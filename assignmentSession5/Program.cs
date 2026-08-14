cc39d3aa644adfe381ee9c08d24618ad0952c761
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-08-14 14:19:24 +0300

    Question02

--- assignmentSession5/Program.cs
+++ assignmentSession5/Program.cs
@@ -5,11 +5,17 @@
         static void Main(string[] args)
         {
             #region Question01
-            Book book = new Book();
-            Console.WriteLine(book.password);// cannot access password because it is private . 
-            // Private members can only be accessed from inside the Book class.
+            //Book book = new Book();
+            //Console.WriteLine(book.password);// cannot access password because it is private . 
+            //// Private members can only be accessed from inside the Book class.
 
             #endregion
+            #region Question02
+            Book book = new Book();
+            Console.WriteLine(book.copiesInStock); // print 5, It compiles because copiesInStock is internal,
+                                                   // and Main is in he same assembly
+            #endregion
+
         }
     }
 }
