830f08784e5531a63a37067292c3ebe792e04019
Ahmed Ehab <104663084+ahmedehab311@users.noreply.github.com>
2026-08-14 14:49:45 +0300

    Question04

--- assignmentSession5/Program.cs
+++ assignmentSession5/Program.cs
@@ -1,9 +1,17 @@
 ﻿namespace assignmentSession5
 {
+    enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
     internal class Program
     {
+
         static void Main(string[] args)
         {
+
             #region Question01
             //Book book = new Book();
             //Console.WriteLine(book.password);// cannot access password because it is private . 
@@ -15,12 +23,18 @@
             //Console.WriteLine(book.copiesInStock); // print 5, It compiles because copiesInStock is internal,
             //                                       // and Main is in he same assembly
             #endregion
-
             #region Question03
-            Book book = new Book();
-            book.Title = "MyTitle";
-            Console.WriteLine(book.Title);
+            //Book book = new Book();
+            //book.Title = "MyTitle";
+            //Console.WriteLine(book.Title);
             #endregion
+            #region Question04
+            Book book = new Book();
+
+            book.Genre = Genre.Science;
+            Console.WriteLine(book.Genre); //Science
+            #endregion
+
         }
     }
 }
